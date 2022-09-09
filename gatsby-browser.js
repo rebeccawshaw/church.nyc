e9396e7d8427ba4d74feefc534abc846883b9c3a
Rebecca Shaw <rwshaw@google.com>
2022-09-09 17:11:50 -0400

    add planning center login

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,3 +5,24 @@
  */
 
 // You can delete this file if you're not using it
+//gatsby-browser.js
+import React from 'react';
+import { Auth0Provider } from '@auth0/auth0-react';
+import { navigate } from 'gatsby';
+
+const onRedirectCallback = (appState) => {
+ // Use Gatsby's navigate method to replace the url
+ navigate(appState?.returnTo || '/', { replace: true });
+};
+
+export const wrapRootElement = ({ element }) => {
+    return (
+        <Auth0Provider
+        domain={"dev-ug8wjlut.us.auth0.com"}
+        clientId={"1kkRlEcKYHuCNRoQNFddVkkw3ERUU4Kp"}
+        redirectUri={window.location.origin}
+        onRedirectCallback={onRedirectCallback}>
+            {element}
+        </Auth0Provider>
+ );
+};
