8baa75f92ea178cdfd29ed5bd49b55d191215a9a
rebeccawshaw <rebeccawshaw@gmail.com>
2022-09-27 11:33:25 -0400

    remove cloudinary and pc components

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,10 +4,8 @@ import { Link } from "gatsby"
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Seo from "../components/common/seo"
-import LoginButton from "../components/login-button";
 import Layout from '../components/layout'
 import InstagramSection from "../components/index/instagramSection";
-import Gallery from '../components/slider';
 import Banner from "../components/index/banner";
 import Vision from "../components/index/vision";
 import UpcomingEvents from "../components/index/upcomingEvents";
@@ -23,9 +21,6 @@ return (
       <UpcomingEvents />
       <GetConnected />
       <InstagramSection />
-          {/* <LoginButton />
-          <Link to="/account">Planning Center account</Link>
-          <Link to="/calendar">Calendar</Link> */}
     </Layout>
   )
 }
